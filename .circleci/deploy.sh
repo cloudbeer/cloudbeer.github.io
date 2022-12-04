9834cbe8f83f87b67f9f664540704e0c940e90d2
Cloudbeer <cloudbeer@gmail.com>
2022-12-05 00:10:40 +0800

    debug11

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,20 +8,21 @@ find . -maxdepth 1  ! -name '.'  ! -name '..' ! -name '_site' ! -name '.git' ! -
 mv _site/* .
 rm -R _site/
 
-git status
 
 # git checkout -f main
 # git pull origin main
  
-git add .
+git add -A .
 git commit --allow-empty -m "Page release ${CIRCLE_BUILD_NUM} from ${CIRCLE_BRANCH}"
 
+
+git status
 # git commit -m "Create new files $(git log source -1 --pretty=%B)"
 
 
 # git remote set-url origin https://cloudbeer:${GITHUB_PWD}@github.com/cloudbeer/cloudbeer.github.io.git
 # git push -f origin main
-git push -q --force https://cloudbeer:${GITHUB_PWD}@github.com/cloudbeer/cloudbeer.github.io.git main
+git push --force https://cloudbeer:${GITHUB_PWD}@github.com/cloudbeer/cloudbeer.github.io.git main
 
 
 echo "deployed successfully"

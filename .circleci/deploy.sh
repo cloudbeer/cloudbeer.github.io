df0c4c8a1249c6e7f82517ed096faee00a89a62f
Cloudbeer <cloudbeer@gmail.com>
2022-12-04 23:52:21 +0800

    rewrite deploy

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,21 +2,26 @@ git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
 
-git checkout main
-git pull origin main
-
 # find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
 
-find . -maxdepth 1 ! -name '_site' -exec rm -rf {} \;
+find . -maxdepth 1  ! -name '.'  ! -name '..' ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
+
+
+# git checkout -f main
+# git pull origin main
+ 
 git add -fA
-git commit -m "Create new files $(git log source -1 --pretty=%B)"
+git commit --allow-empty -m "Page release ${CIRCLE_BUILD_NUM} from ${CIRCLE_BRANCH}"
+
+# git commit -m "Create new files $(git log source -1 --pretty=%B)"
 
 
-git remote set-url origin https://cloudbeer:${GITHUB_PWD}@github.com/cloudbeer/cloudbeer.github.io.git
+# git remote set-url origin https://cloudbeer:${GITHUB_PWD}@github.com/cloudbeer/cloudbeer.github.io.git
+# git push -f origin main
+git push -q --force https://cloudbeer:${GITHUB_PWD}@github.com/cloudbeer/cloudbeer.github.io.git main
 
-git push -f origin main
 
 echo "deployed successfully"

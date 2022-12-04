a23ed690d1ce4d38fd5bd3012b271bed135bbe9c
Cloudbeer <cloudbeer@gmail.com>
2022-12-04 23:32:11 +0800

    try

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,17 +2,18 @@ git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
+git checkout main
+git pull origin main
+
+# find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
+
+find . -maxdepth 1 ! -name '_site' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
-
 git add -fA
-git commit --allow-empty -m "$(git log source -1 --pretty=%B)"
+git commit -m "Create new files $(git log source -1 --pretty=%B)"
 
-
-git checkout main
-# git pull origin main
 
 git remote set-url origin https://cloudbeer:${GITHUB_PWD}@github.com/cloudbeer/cloudbeer.github.io.git
 
